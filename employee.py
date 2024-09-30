a6e829484e509052b7b8a18d9e33360e86dea620
Mark Peters <mapeters101@earthlink.net>
2024-09-30 19:29:27 -0400

    Lesson015  Replace name with first_name and last_name.

--- employee.py
+++ employee.py
@@ -1,6 +1,7 @@
 class Employee:
-    def __init__(self, name, salary):
-        self.name = name
+    def __init__(self, first_name, last_name, salary):
+        self.first_name = first_name
+        self.last_name = last_name
         self.salary = salary
 
 class Manager(Employee):
