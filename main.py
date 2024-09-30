d59b145a941366170e938b67d6296464aa3b72c5
Mark Peters <mapeters101@earthlink.net>
2024-09-30 10:30:01 -0400

    Lesson010: Enter names and salaries.  Print to screen

---
+++ main.py
@@ -0,0 +1,8 @@
+names = ["Vera", "Chuck", "Samantha", "Roberto", "Joe", "Dave", "Tina", "Ringo"]
+salaries = [2000, 18000, 18000, 21000, 2000, 2200, 2300, 1900]
+
+for n in names:
+    print(n)
+
+for s in salaries:
+    print(s)
