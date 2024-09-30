86e681c41477c3713b7e35176048b3a9ce41ffe7
Mark Peters <mapeters101@earthlink.net>
2024-09-30 19:16:58 -0400

    Lesson014  Refactor: Create employee.py and move all employee classes from main.py to employee.py.

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-class Employee:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
 
-class Manager(Employee):
-    job_title = "Manager"
-
-class Cook(Employee):
-    job_title = "Cook"
-
-class Attendant(Employee):
-    job_title = "Station Attendant"
-
-class Mechanic(Employee):
-    job_title = "Mechanic"
 
 employees = [
     Manager("Vera", 2000),
