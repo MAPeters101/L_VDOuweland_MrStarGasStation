529dbac98b6a93c4f5e9093bf25fdadd6d8d2bbf
Mark Peters <mapeters101@earthlink.net>
2024-09-30 15:30:29 -0400

    Lesson011: Remove individual e. Create list of employees.  Loop through list and print name & salary.

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ class Employee:
         self.name = name
         self.salary = salary
 
-e = Employee("Vera", 2000)
-print(f"{e.name}, {e.salary}")
+employees = [
+    Employee("Vera", 2000),
+    Employee("Chuck", 1800),
+    Employee("Samantha", 1800),
+    Employee("Roberto", 2100),
+    Employee("Dave", 2200),
+    Employee("Tina", 2300),
+    Employee("Ringo", 1900)
+]
+
+for e in employees:
+    print(f"{e.name}, ${e.salary}")
