69714fe22f94aff08cbc9f75e4f8d1512e903819
Mark Peters <mapeters101@earthlink.net>
2024-09-30 15:48:23 -0400

    Lesson012: Add job_title to Employee class.  Add title to object creation list.  Add job_title to print statement.

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 class Employee:
-    def __init__(self, name, salary):
+    def __init__(self, name, salary, job_title):
         self.name = name
         self.salary = salary
+        self.job_title = job_title
 
 employees = [
-    Employee("Vera", 2000),
-    Employee("Chuck", 1800),
-    Employee("Samantha", 1800),
-    Employee("Roberto", 2100),
-    Employee("Dave", 2200),
-    Employee("Tina", 2300),
-    Employee("Ringo", 1900)
+    Employee("Vera", 2000, "Manager"),
+    Employee("Chuck", 1800, "Attendant"),
+    Employee("Samantha", 1800, "Attendant"),
+    Employee("Roberto", 2100, "Cook"),
+    Employee("Dave", 2200, "Car Repair"),
+    Employee("Tina", 2300, "Car Repair"),
+    Employee("Ringo", 1900, "Car Repair")
 ]
 
 for e in employees:
-    print(f"{e.name}, ${e.salary}")
+    print(f"{e.name}, ${e.salary}, {e.job_title}")
 
