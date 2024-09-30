b0e565dc7787d20d51b21443720ef85d55a29e37
Mark Peters <mapeters101@earthlink.net>
2024-09-30 16:01:00 -0400

    Lesson013: Inheritance:  Create Mechanic class and set job_title = Mechanic.  Remove job_title from Employee class. Remove job titles from list creation.  Change Employee creation to Mechanic

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ class Employee:
     def __init__(self, name, salary, job_title):
         self.name = name
         self.salary = salary
-        self.job_title = job_title
+
+
+class Mechanic(Employee):
+    job_title = "Mechanic"
 
 employees = [
-    Employee("Vera", 2000, "Manager"),
-    Employee("Chuck", 1800, "Station Attendant"),
-    Employee("Samantha", 1800, "Station Attendant"),
-    Employee("Roberto", 2100, "Cook"),
-    Employee("Dave", 2200, "Mechanic"),
-    Employee("Tina", 2300, "Mechanic"),
-    Employee("Ringo", 1900, "Mechanic")
+    Employee("Vera", 2000),
+    Employee("Chuck", 1800),
+    Employee("Samantha", 1800),
+    Employee("Roberto", 2100),
+    Mechanic("Dave", 2200),
+    Mechanic("Tina", 2300),
+    Mechanic("Ringo", 1900)
 ]
 
 for e in employees:
