6c00508b23ff8ebdb9d231110e353727ce8e21db
Mark Peters <mapeters101@earthlink.net>
2024-09-30 10:34:56 -0400

    Lesson010: Determine len of names.  Print list of name, salary.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 names = ["Vera", "Chuck", "Samantha", "Roberto", "Joe", "Dave", "Tina", "Ringo"]
 salaries = [2000, 18000, 18000, 21000, 2000, 2200, 2300, 1900]
 
-for n in names:
-    print(n)
-
-for s in salaries:
-    print(s)
+count = len(names)
+for c in range(count):
+    print(f"{names[c]}, %{salaries[c]}")
