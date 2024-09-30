4138506c18a3f14b2f63ac0ef5c37249c5907144
Mark Peters <mapeters101@earthlink.net>
2024-09-30 15:40:30 -0400

    Lesson011: Remove Wendy from list.  Remove problematic if structure.

--- main.py
+++ main.py
@@ -10,17 +10,9 @@ employees = [
     Employee("Roberto", 2100),
     Employee("Dave", 2200),
     Employee("Tina", 2300),
-    Employee("Ringo", 1900),
-    Employee("Wendy", 2000)
+    Employee("Ringo", 1900)
 ]
 
 for e in employees:
-    if e.name == "Vera":
-        print(f"{e.name}, ${e.salary}, Manager")
-    if e.name == "Chuck" or e.name == "Samantha":
-        print(f"{e.name}, ${e.salary}, Attendant")
-    if e.name == "Roberto":
-        print(f"{e.name}, ${e.salary}, Cook")
-    if e.name == "Dave" or e.name == "Tina" or e.name == "Ringo":
-        print(f"{e.name}, ${e.salary}, Car Repair")
+    print(f"{e.name}, ${e.salary}")
 
