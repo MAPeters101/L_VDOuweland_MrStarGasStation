8fcdf57825a2aa90e82acff9bd20b31a857d773b
Mark Peters <mapeters101@earthlink.net>
2024-10-01 10:18:25 -0400

    Lesson020:  Composition:  In ScheduleReport, replace times with e.shift.get_shift_info().

--- reporting.py
+++ reporting.py
@@ -24,7 +24,7 @@ class ScheduleReport(Report):
         print("Schedule")
         print("========")
         for e in self._emp_list:
-            print(f"{e.get_full_name()}, {e.start_time:%H:%M} to {e.end_time:%H:%M}")
+            print(f"{e.get_full_name()}, {e.shift.get_shift_info()}")
 
 
 
